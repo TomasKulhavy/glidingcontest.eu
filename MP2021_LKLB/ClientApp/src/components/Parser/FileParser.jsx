9486d5a45c26526d606a51b5758a409276fd9af8
Tomáš Kulhavý <43739618+TomasKulhavy@users.noreply.github.com>
2021-02-26 15:03:20 +0100

    Delete files

--- MP2021_LKLB/ClientApp/src/components/Parser/FileParser.jsx
+++
@@ -1,6 +0,0 @@
-var RE_L = /^L.*(?=R1)R1=(.*)km,A1.*$/;
-
-var str = 'LNAVOZN=0,Style=3,R1=0.500km,A1=45,R2=0.000km,A2=0,A12=51.8';
-var result = str.match(RE_L);
-console.log(result);
-// returns true
